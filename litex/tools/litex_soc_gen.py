655a38315f2d3b9bf253125cc8d9bb7bc9ba83d1
Haggui Naouel <naouel.haggui@insa-rennes.fr>
2024-07-05 10:08:11 +0200

    update litex_soc_gen.py file

--- litex/tools/litex_soc_gen.py
+++ litex/tools/litex_soc_gen.py
@@ -54,6 +54,7 @@ def get_debug_ios(debug_width=8):
 
 class Platform(GenericPlatform):
     def build(self, fragment, build_dir, build_name, **kwargs):
+        print('build_dir : ', build_dir)
         os.makedirs(build_dir, exist_ok=True)
         os.chdir(build_dir)
         conv_output = self.get_verilog(fragment, name=build_name)
@@ -62,12 +63,15 @@ class Platform(GenericPlatform):
 # LiteX SoC Generator ------------------------------------------------------------------------------
 
 class LiteXSoCGenerator(SoCMini):
-    def __init__(self, name="litex_soc", sys_clk_freq=int(50e6), **kwargs):
+    #def __init__(self, name="litex_soc", sys_clk_freq=int(50e6), **kwargs):
+    def __init__(self, n_master_i, n_slave_i, name="litex_soc", sys_clk_freq=int(50e6), **kwargs):
         # Platform ---------------------------------------------------------------------------------
+        print("#"*20, 'start platform')
         platform = Platform(device="", io=get_common_ios())
+        print("#"*20, 'END platform')
         platform.name = name
         platform.add_extension(get_uart_ios())
-
+        self.kwargs = kwargs #added by naouel
         # CRG --------------------------------------------------------------------------------------
         self.submodules.crg = CRG(
             clk = platform.request("clk"),
@@ -75,44 +79,57 @@ class LiteXSoCGenerator(SoCMini):
         )
 
         # SoC --------------------------------------------------------------------------------------
-        if kwargs["uart_name"] == "serial":
-            kwargs["uart_name"] = "uart"
+        #if kwargs["uart_name"] == "serial":
+            #kwargs["uart_name"] = "uart"
+        if self.kwargs["uart_name"] == "serial":
+            self.kwargs["uart_name"] = "uart"
         SoCCore.__init__(self, platform, clk_freq=sys_clk_freq, ident=f"LiteX standalone SoC - {name}", **kwargs)
+#####changed part#######################################################################################
+        # MMAP Master Interface --------------------------------------------------------------------
+        # FIXME: Allow Region configuration.
+        print("#"*20, 'befor index n_master_i')
+        for n in range(n_master_i):
+            print("#"*20, 'index n_master_i', n)
+            origins = [0xa000_0000, 0xb000_0000, 0xc000_0000, 0xd000_0000]
+            self.add_master_interface(mmap_id=n, origin=origins[n])
 
         # MMAP Slave Interface ---------------------------------------------------------------------
         s_bus = {
-            "wishbone" : wishbone.Interface(),
-            "axi-lite" : axi.AXILiteInterface(),
+            "wishbone": wishbone.Interface(),
+            "axi-lite": axi.AXILiteInterface(),
 
-        }[kwargs["bus_standard"]]
-        self.bus.add_master(name="mmap_s", master=s_bus)
-        platform.add_extension(s_bus.get_ios("mmap_s"))
-        wb_pads = platform.request("mmap_s")
-        self.comb += s_bus.connect_to_pads(wb_pads, mode="slave")
-
-        # MMAP Master Interface --------------------------------------------------------------------
-        # FIXME: Allow Region configuration.
-        m_bus = {
-            "wishbone" : wishbone.Interface(),
-            "axi-lite" : axi.AXILiteInterface(),
-
-        }[kwargs["bus_standard"]]
-        wb_region = SoCRegion(origin=0xa000_0000, size=0x1000_0000, cached=False) # FIXME.
-        self.bus.add_slave(name="mmap_m", slave=m_bus, region=wb_region)
-        platform.add_extension(m_bus.get_ios("mmap_m"))
-        wb_pads = platform.request("mmap_m")
-        self.comb += m_bus.connect_to_pads(wb_pads, mode="master")
+        }[self.kwargs["bus_standard"]]
+        for i in range(n_slave_i):
+            i_name = f"mmap_s_{i}"
+            self.bus.add_master(name=i_name, master=s_bus)
+            platform.add_extension(s_bus.get_ios(i_name))
+            wb_pads = platform.request(i_name)
+            self.comb += s_bus.connect_to_pads(wb_pads, mode="slave")
 
         # Debug ------------------------------------------------------------------------------------
         platform.add_extension(get_debug_ios())
         debug_pads = platform.request("debug")
         self.comb += [
             # Export Signal(s) for debug.
-            debug_pads[0].eq(0), # 0.
-            debug_pads[1].eq(1), # 1.
+            debug_pads[0].eq(0),  # 0.
+            debug_pads[1].eq(1),  # 1.
             # Etc...
         ]
+        # self.add_timer()
 
+    def add_master_interface(self, mmap_id, origin=0xa0000000):
+        assert isinstance(mmap_id, int)
+        # MMAP Master Interface --------------------------------------------------------------------
+        m_bus = {
+            "wishbone": wishbone.Interface(),
+            "axi-lite": axi.AXILiteInterface(),
+        }[self.kwargs["bus_standard"]]
+        wb_region = SoCRegion(origin=origin, size=0x10000000, cached=False)
+        self.bus.add_slave(name="mmap_m_{}".format(mmap_id), slave=m_bus, region=wb_region)
+        self.platform.add_extension(m_bus.get_ios("mmap_m_{}".format(mmap_id)))
+        wb_pads = self.platform.request("mmap_m_{}".format(mmap_id))
+        self.comb += m_bus.connect_to_pads(wb_pads, mode="master")
+#####fin changed part#######################################################################################
 # Build --------------------------------------------------------------------------------------------
 def main():
     # Arguments.
@@ -120,6 +137,8 @@ def main():
     parser = LiteXSoCArgumentParser(description="LiteX standalone SoC generator")
     target_group = parser.add_argument_group(title="Generator options")
     target_group.add_argument("--name",          default="litex_soc", help="SoC Name.")
+    target_group.add_argument("--n_master_i", default=int(2), help="Number of master interfaces.")
+    target_group.add_argument("--n_slave_i", default=int(0), help="Number of slave interfaces.")
     target_group.add_argument("--build",         action="store_true", help="Build SoC.")
     target_group.add_argument("--sys-clk-freq",  default=int(50e6),   help="System clock frequency.")
     builder_args(parser)
@@ -130,6 +149,8 @@ def main():
     soc = LiteXSoCGenerator(
         name         = args.name,
         sys_clk_freq = int(float(args.sys_clk_freq)),
+        n_master_i=int(args.n_master_i),
+        n_slave_i=int(args.n_slave_i),
         **soc_core_argdict(args)
     )
 
